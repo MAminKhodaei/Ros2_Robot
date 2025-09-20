478a7b5333e1ca088f8d15e019dfbcbb77823121
MAminKhodaei <amin.khodaei1032@gmail.com>
2025-09-20 08:25:04 +0330

    fix setup.py

--- ros2_ws/src/robot_bringup/setup.py
+++ ros2_ws/src/robot_bringup/setup.py
@@ -1,29 +1,29 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'robot_bringup'
 
 setup(
     name=package_name,
     version='0.0.1',
-    packages=find_packages(exclude=['test']),
+    # تغییر اصلی اینجاست: ما به طور مستقیم نام پکیج را مشخص می‌کنیم
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # اضافه کردن فایل لانچ به پکیج
         ('share/' + package_name + '/launch', ['launch/robot.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='MohammadAminKhodaei',
-    maintainer_email='amin.khodaei1032@gmail.com',
+    maintainer='amin',
+    maintainer_email='amin@example.com',
     description='The main package to bring up the robot',
     license='Apache-2.0',
-    tests_require=['pytest'],
+    # گزینه 'tests_require' حذف شد تا اخطار مزاحم نمایش داده نشود
     entry_points={
         'console_scripts': [
             'camera_node = robot_bringup.camera_node:main',
-            'serial_bridge_node = robot_bringup.serial_bridge_node:main', 
+            'serial_bridge_node = robot_bringup.serial_bridge_node:main',
         ],
     },
 )
