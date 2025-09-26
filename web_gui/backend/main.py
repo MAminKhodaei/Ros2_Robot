8e4bbd9feb4bfd59dfce2b736d48e420dbd77b55
MAminKhodaei <amin.khodaei1032@gmail.com>
2025-09-26 10:30:01 +0330

    cv2

--- web_gui/backend/main.py
+++ web_gui/backend/main.py
@@ -40,7 +40,7 @@ class RosBridgeNode(Node):
         try:
             # این کد با فرمت ارسالی از v4l2_camera هماهنگ است
             cv_image_rgb = self.bridge.imgmsg_to_cv2(msg, "rgb8")
-            cv_image_bgr = cv.cvtColor(cv_image_rgb, cv.COLOR_RGB2BGR)
+            cv_image_bgr = cv2.cvtColor(cv_image_rgb, cv2.COLOR_RGB2BGR)
             _, buffer = cv2.imencode('.jpg', cv_image_bgr)
             jpg_as_text = base64.b64encode(buffer).decode('utf-8')
             asyncio.run(self.sio.emit('video_update', jpg_as_text))
